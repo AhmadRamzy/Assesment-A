1b8fac39a180226089996302892714d11e806f8d
AhmadRamzy <152113637+AhmadRamzy@users.noreply.github.com>
2023-12-04 11:46:18 +0700

    Update search.js

--- search.js
+++ search.js
@@ -1,26 +1,48 @@
 $(document).ready(function() {
-    // Mengatur animasi untuk tampilan input pencarian saat ikon pencarian diklik
+    var isSearching = false; // Menandai apakah sedang dalam proses pencarian
+
     $('#searchIcon').on('click', function() {
         $('#searchInput').fadeToggle('fast');
-        $('#searchInput').focus(); // memberi fokus pada input setelah muncul
+        $('#searchInput').focus();
+        isSearching = true; // Menandai bahwa sedang melakukan pencarian
     });
 
-    // Fungsi pencarian item
     $('#searchInput').on('keyup', function() {
         var value = $(this).val().toLowerCase();
-        $('.isi').filter(function() {
-            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+        var found = false;
+
+        $('.isi').each(function() {
+            var text = $(this).text().toLowerCase();
+            var matched = text.indexOf(value) > -1;
+            $(this).toggle(matched);
+            
+            if (matched) {
+                found = true;
+            }
         });
+
+        if (!found) {
+            $('#notFoundMessage').text('Barang tidak ditemukan');
+        } else {
+            $('#notFoundMessage').text('');
+        }
     });
 
-    // Menyembunyikan input pencarian saat kehilangan fokus atau tombol esc ditekan
     $('#searchInput').on('focusout', function() {
-        $(this).fadeOut('fast');
+        if (isSearching) {
+            $(this).fadeOut('fast');
+            $('.isi').show(); // Mengosongkan nilai input saat focus keluar dari area pencarian
+            $('#notFoundMessage').text('');
+            isSearching = false; // Menandai bahwa pencarian selesai
+        }
     });
 
     $(document).on('keydown', function(event) {
         if (event.key === 'Escape') {
             $('#searchInput').fadeOut('fast');
+            $('#searchInput').val(''); // Mengosongkan nilai input saat menekan tombol Escape
+            $('#notFoundMessage').text('');
+            isSearching = false; // Menandai bahwa pencarian selesai
         }
     });
 });
